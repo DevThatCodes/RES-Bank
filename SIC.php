65c5de3de0019a809d2c9a65b4471874c21ed99b
DevThatCodes <1steverchessguy@gmail.com>
2024-02-12 20:53:06 -0500

    added other stuff

--- SIC.php
+++ SIC.php
@@ -14,12 +14,13 @@
         <h1>RES Bank</h1>
         <a href="index.html">home</a>
     </div>
-    <span style="text-align: right; user-select: none; background-color: rgb(122, 232, 255);" id="NBT">Toggle NavBar</span>
+    <span style="text-align: right; user-select: none; background-color: rgb(122, 232, 255);" id="NBT">Toggle
+        NavBar</span>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <br><br><br><br><br><br>
     <script>
-        $(document).ready(function(){
-            $("#NBT").click(function(){
+        $(document).ready(function () {
+            $("#NBT").click(function () {
                 $(".header").slideToggle(1000);
             })
         });
